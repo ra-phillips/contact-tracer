5f5cb548dd6f75976b66bd36584f804886e98d2f
ra-phillips <72104229+ra-phillips@users.noreply.github.com>
2021-03-16 17:06:56 -0400

    Update server app

--- app.js
+++ app.js
@@ -4,25 +4,45 @@ const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, './client/build')));
+
+
 //Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+//production mode
+if(process.env.NODE_ENV === 'production') { 
+     app.use(express.static(path.join(__dirname, './client/build')));  
+     app.get('*', (req, res) => {    
+         res.sendfile(path.join(__dirname = './client/build/index.html'));  
+        }
+    )
+}
+
+//build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+// app.get('/',(req,res)=>{
+//     res.send({message: "We are on home"});
+// });
+
 
 //Import Routes
-const clientsRoute = require('./routes/clients');
-const employeesRoute = require('./routes/employees');
-const employeesLogRoute = require('./routes/employeeLogs');
+// const clientsRoute = require('./routes/clients');
+// const employeesRoute = require('./routes/employees');
+// const employeesLogRoute = require('./routes/employeeLogs');
 
-app.get('/',(req,res)=>{
-    res.send({message: "We are on home"});
-});
-
-app.use('/clients',clientsRoute);
-app.use('/employees',employeesRoute);
-app.use('/employeesLog',employeesLogRoute);
+// app.use('/clients',clientsRoute);
+// app.use('/employees',employeesRoute);
+// app.use('/employeesLog',employeesLogRoute);
 
 
 //Connect to DB jY1brHxgg3zbOgNk
@@ -33,7 +53,8 @@ mongoose.connect('mongodb+srv://RPhillips:'
     () => console.log("MongoDB database connection established successfully")
 );
 
-//Start Server
-app.listen(port, () =>{
-    console.log(`Server is running on port: ${port}`);
+//start server
+app.listen(port, (req, res) => {  
+    console.log( `server listening on port: ${port}`);
 })
+

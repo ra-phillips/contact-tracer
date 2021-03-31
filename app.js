42b5887b76fea2b96061094a5ef4b771d3d1b993
ra-phillips <72104229+ra-phillips@users.noreply.github.com>
2021-03-31 10:45:28 -0400

    Update server & package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv/config');
 
 const app = express();
@@ -25,12 +26,13 @@ app.use('/clients',clientsRoute);
 app.use('/employees',employeesRoute);
 app.use('/employeesLog',employeesLogRoute);
 
+//Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
